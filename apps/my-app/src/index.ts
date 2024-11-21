de187a4b485491e6efc9eb8a5d3e23a0465872e9
neoandmatrix <tusharannand@gmail.com>
2024-11-21 21:48:57 +0530

    tesiting kv in prod

--- apps/my-app/src/index.ts
+++ apps/my-app/src/index.ts
@@ -1,9 +1,26 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+type Bindings = {
+  CACHE : KVNamespace
+}
 
-app.get('/', (c) => {
-  return c.text('Hello from Hono!')
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/:username', async (c) => {
+  const username = c.req.param("username")
+  const cachedResponse = await c.env.CACHE.get(username,"json");
+  if (cachedResponse) {
+    console.log(`cache hit for ${username}`)
+    return c.json(cachedResponse);
+  }
+  const response = await fetch(`https://api.github.com/users/${username}/repos`, {
+    headers: {
+      "User-Agent": "CF-worker"
+    }
+  })
+  const data: any = await response.json()
+  await c.env.CACHE.put(username,JSON.stringify(data))
+  return c.json(data)
 })
 
 export default app
