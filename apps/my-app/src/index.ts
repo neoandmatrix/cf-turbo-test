80c7fed7d2f455a071678f01a895a787deb19978
neoandmatrix <tusharannand@gmail.com>
2024-12-20 14:04:53 +0530

    fixing zod

--- apps/my-app/src/index.ts
+++ apps/my-app/src/index.ts
@@ -1,30 +1,34 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 import { z } from "zod";
 
 type Bindings = {
-  CACHE : KVNamespace
-}
+  CACHE: KVNamespace;
+};
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('/:username', async (c) => {
-  const username = c.req.param("username")
-  const usernameValidation = z.string().nonempty().safeParse(username).success
-  if (usernameValidation === false) {
-    return c.json({error: "Invalid username"})
-  }
-  const cachedResponse = await c.env.CACHE.get(username,"json");
-  if (cachedResponse) {
-    return c.json(cachedResponse);
-  }
-  const response = await fetch(`https://api.github.com/users/${username}/repos`, {
-    headers: {
-      "User-Agent": "CF-worker"
+app.get("/:username", async (c) => {
+  const username = c.req.param("username");
+  const usernameValidation = z.string();
+  if (usernameValidation.safeParse(username).success === false) {
+    return c.json({ error: "Invalid username" });
+  } else {
+    const cachedResponse = await c.env.CACHE.get(username, "json");
+    if (cachedResponse) {
+      return c.json(cachedResponse);
     }
-  })
-  const data: any = await response.json()
-  await c.env.CACHE.put(username,JSON.stringify(data))
-  return c.json(data)
-})
+    const response = await fetch(
+      `https://api.github.com/users/${username}/repos`,
+      {
+        headers: {
+          "User-Agent": "CF-worker",
+        },
+      }
+    );
+    const data: any = await response.json();
+    await c.env.CACHE.put(username, JSON.stringify(data));
+    return c.json(data);
+  }
+});
 
-export default app
+export default app;
