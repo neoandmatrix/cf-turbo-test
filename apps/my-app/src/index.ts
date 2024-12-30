f8752e27a8eba3d2c08596f0f1b8dd95fb713b06
neoandmatrix <tusharannand@gmail.com>
2024-12-30 16:04:52 +0530

    fixed:zod validator

--- apps/my-app/src/index.ts
+++ apps/my-app/src/index.ts
@@ -15,33 +15,28 @@ app.get("/error", (c) => {
   throw new Error("This is an error");
 })
 
-const usernameValidation = z.string();
+const usernameValidation = z.object({username : z.string()});
 
 app.get("/github/:username",zValidator('param',usernameValidation) ,async (c) => {
   const username = c.req.param("username");
   
-  if (usernameValidation.safeParse(username).success === false) {
-    console.log({ username });
-    return c.json({ error: "Invalid username" });
-  } else {
-    const cachedResponse = await c.env.CACHE.get(username, "json");
-    if (cachedResponse) {
-      console.log({ stauts : "success" });
-      return c.json(cachedResponse);
-    }
-    console.log({action : "fetching from github"});
-    const response = await fetch(
-      `https://api.github.com/users/${username}/repos`,
-      {
-        headers: {
-          "User-Agent": "CF-worker",
-        },
-      }
-    );
-    const data: any = await response.json();
-    await c.env.CACHE.put(username, JSON.stringify(data));
-    return c.json(data);
+  const cachedResponse = await c.env.CACHE.get(username, "json");
+  if (cachedResponse) {
+    console.log({ stauts : "success" });
+    return c.json(cachedResponse);
   }
+  console.log({action : "fetching from github"});
+  const response = await fetch(
+    `https://api.github.com/users/${username}/repos`,
+    {
+      headers: {
+        "User-Agent": "CF-worker",
+      },
+    }
+  );
+  const data: any = await response.json();
+  await c.env.CACHE.put(username, JSON.stringify(data));
+  return c.json(data);
 });
 
 
